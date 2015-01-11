d746cd07d0dc689e0c8a7e48b501a8046c00b4f8
Kyle Huggins <epiquiem@users.noreply.github.com>
2015-01-10 21:57:07 -0600

    Update RunForRoot.sh

--- RunForRoot.sh
+++ RunForRoot.sh
@@ -2,21 +2,21 @@
 E="echo "[*]"
 platform=`uname`
 if [ $(uname -p) = 'powerpc' ]; then
-    $E PowerPC architecture is not supported."
-    exit 1
+      $E PowerPC architecture is not supported."
+       exit 1
 fi
 if [ "$platform" = 'Darwin' ]; then
-    adb="./files/adb.osx"
-    version="OSx"
+        adb="./files/adb.osx"
+       version="OSx"
 else
-    adb="./files/adb.linux"
-    version="Linux"
+        adb="./files/adb.linux"
+        version="Linux"
 fi
 if [ -f $adb ]
   then
     fi
 else
-    $E ADB not found"
-    exit 1
+       $E ADB not found"
+        exit 1
 fi
 chmod +x $adb
